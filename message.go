08024c1befff5af29016a5a70e924badb9949810
Ingar Shu <ingar@carbonfive.com>
2015-02-19 11:02:03 -0800

    Added example quick start comment

--- message.go
+++ message.go
@@ -1,3 +1,27 @@
+/*
+A Bot Framework
+
+Example:
+
+	package main
+
+	import (
+		"fmt"
+		"github.com/ingar/barglebot"
+		"github.com/ingar/barglebot/transport/slack"
+		"os"
+	)
+
+	func main() {
+		incomingMessages := make(chan barglebot.Message)
+		slack.Connect(os.Getenv("SLACK_BOT_API_KEY"), incomingMessages)
+
+		for {
+			message := <-incomingMessages
+			message.Respond(fmt.Sprintf("You said: '%s'", message.Text()))
+		}
+	}
+*/
 package barglebot
 
 type Message interface {
